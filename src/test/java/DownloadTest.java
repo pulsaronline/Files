297eea706f980e33a0a5a0ae9a5d6a1426319156
Pulsaronline <beslite@gmail.com>
2021-04-01 23:46:16 +0300

    utils added

--- src/test/java/DownloadTest.java
+++ src/test/java/DownloadTest.java
@@ -1,23 +1,30 @@
 import com.codeborne.selenide.Configuration;
+
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import utils.Files;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+
 
 public class DownloadTest {
     @Test
-    void downloadTest() throws FileNotFoundException {
+    void downloadTest() throws IOException {
         Configuration.downloadsFolder = "./downloads";
 
         open("https://github.com/selenide/selenide/blob/master/README.md");
-        File dowloadedFile = $("#raw-url").download();
-        File dowloaded1File = $("#raw-url").download();
-
-// Selenide = UI Testing Framework powered by Selenium WebDriver
+        File downloadedFile = $("#raw-url").download();
+        String fileContent = Files.readTextFromFile(downloadedFile);
+        //assertTrue(fileContent.contains("Selenide = UI Testing Framework powered by Selenium WebDriver"));
+        assertThat(fileContent, containsString("Selenide = UI Testing Framework powered by Selenium WebDriver"));
     }
 
     @Test
---
+++ src/test/java/utils/Files.java
@@ -0,0 +1,14 @@
+package utils;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class Files {
+public static String readTextFromFile(File file) throws IOException {
+    return FileUtils.readFileToString(file, StandardCharsets.UTF_8);
+}
+
+}
