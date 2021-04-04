d59f02aaf6190a78561e3cd46fb10f2e4dd1c36c
Pulsaronline <beslite@gmail.com>
2021-04-04 14:08:05 +0300

    zip files added

---
+++ src/test/java/tests/ZipFileTests.java
@@ -0,0 +1,31 @@
+package tests;
+
+import com.codeborne.xlstest.XLS;
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static utils.Files.*;
+import static utils.Zip.unzip;
+
+public class ZipFileTests {
+    @Test
+    void zipTest() throws IOException, ZipException {
+        String zipFilePath = "./src/test/resources/files/1.zip";
+        String unzipFolderPath = "./src/test/resources/files/unzip";
+        String zipPassword = "";
+        String unzipTxtFilePath = "./src/test/resources/files/unzip/1.txt";
+
+        String expectedData = "hello qa.guru students!";
+
+
+            unzip(zipFilePath, unzipFolderPath, zipPassword);
+
+            String actualData = readTextFromPath(unzipTxtFilePath);
+            assertThat(actualData, containsString(expectedData));
+    }
+}
