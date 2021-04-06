2bcb8b837116c5c7c77e5e68b89f385efbfbc2c5
Pulsaronline <beslite@gmail.com>
2021-04-06 11:10:10 +0300

    code clean 3

--- src/test/java/tests/DownloadTest.java
+++ src/test/java/tests/DownloadTest.java
@@ -1,23 +1,28 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
-
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import utils.Files;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 
 // DOC: https://selenide.org/2019/12/10/advent-calendar-download-files/
 public class DownloadTest {
+
+    private String getSessionId() {
+        return ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
+    }
+
     @Test
     void downloadTest() throws IOException {
         Configuration.downloadsFolder = "./downloads";
@@ -26,7 +31,6 @@ public class DownloadTest {
         File downloadedFile = $("#raw-url").download();
         String fileContent = Files.readTextFromFile(downloadedFile);
         assertThat(fileContent, containsString("Selenide = UI Testing Framework powered by Selenium WebDriver"));
-        //Files.deleteFile("./downloads/9fd47b30-c2eb-4e3d-84c9-9be63a311adf/README.md");
     }
 
     @Test
