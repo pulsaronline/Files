8694994bfe572ad885dd69e2c5460fee699c5109
Wolary <Ktrnjh19>
2021-04-11 20:00:04 +0300

    Немного поправил верстку и кавычки.

--- src/test/java/utils/Zip.java
+++ src/test/java/utils/Zip.java
@@ -4,15 +4,15 @@ import net.lingala.zip4j.core.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
 
 public class Zip {
-public static void unzip(String path, String unzipPath, String password) throws ZipException {
-    ZipFile zipFile = new ZipFile(path);
-    if (zipFile.isEncrypted()) {
-        zipFile.setPassword(password);
+    public static void unzip(String path, String unzipPath, String password) throws ZipException {
+        ZipFile zipFile = new ZipFile(path);
+        if (zipFile.isEncrypted()) {
+            zipFile.setPassword(password);
+        }
+        zipFile.extractAll(unzipPath);
     }
-    zipFile.extractAll(unzipPath);
-}
 
-public static void unzip(String path, String unzipPath) throws ZipException {
-        unzip(path, unzipPath,"");
+    public static void unzip(String path, String unzipPath) throws ZipException {
+        unzip(path, unzipPath, "");
     }
 }
